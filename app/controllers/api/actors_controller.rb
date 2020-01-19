e29c70cfed405bc8ccc364b6cea034a091d11df9
christine <credhead89@gmail.com>
2020-01-19 18:28:40 -0500

    created seeds and models and practice query methods to my movie-app

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -3,5 +3,13 @@ class Api::ActorsController < ApplicationController
     @actor = Actor.last
     render "actor.json.jb"
   end
+  def movie_method
+    @individual_movie = Movie.all
+    render "movie.json.jb"
+  end
+  def movie_method
+    @movie = Movie.take(2)
+    render "movie.json.jb" 
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/actor_url" => "actors#actor_action"
+    get"/movie_url" => "actors#movie_method"
   end
 end
