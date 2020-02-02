389ec1cc7f275a0b8ed64babbe1652099b78df27
christine <credhead89@gmail.com>
2020-02-02 17:55:21 -0500

    Create new genre model and created associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
   has_many :actors
-  validates :title, length: { minimum: 2 }
-  validates :plot, length: { maximum: 500 }
-  validates :title, :director, presence: true
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres 
+  # validates :title, length: { minimum: 2 }
+  # validates :plot, length: { maximum: 500 }
+  # validates :title, :director, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-actor = Actor.create(first_name: "Jessie", last_name: "williams", known_for: "Grey's Anatomy")
+# actor = Actor.create(first_name: "Jessie", last_name: "williams", known_for: "Grey's Anatomy")
 
-movie = Movie.new(title: "Ratatouille", year: 2007, plot: "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession. He moves to Paris to follow his dream, and with the help of hapless garbage boy Linguini he puts his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually gets the chance to prove his culinary abilities to a great food critic but is the food good? A Pixar animation.")
-movie.save
+# movie = Movie.new(title: "Ratatouille", year: 2007, plot: "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession. He moves to Paris to follow his dream, and with the help of hapless garbage boy Linguini he puts his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually gets the chance to prove his culinary abilities to a great food critic but is the food good? A Pixar animation.")
+# movie.save
 
+# Genre.create!([
+#   {name: "thriller"},
+#   {name: "romcom"},
+#   {name: "action"},
+#   {name: "science fiction"}
+# ])
+
+MovieGenre.create!([
+  {movie_id: 1},
+  {genre_id: 1}
+])
