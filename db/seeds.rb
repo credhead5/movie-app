1f28c8db5f3f62950cfac93d3626ace9f24262f8
christine <credhead89@gmail.com>
2020-02-19 08:25:01 -0500

    running data test in heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@
 # movie = Movie.new(title: "Ratatouille", year: 2007, plot: "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession. He moves to Paris to follow his dream, and with the help of hapless garbage boy Linguini he puts his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually gets the chance to prove his culinary abilities to a great food critic but is the food good? A Pixar animation.")
 # movie.save
 
-# Genre.create!([
-#   {name: "thriller"},
-#   {name: "romcom"},
-#   {name: "action"},
-#   {name: "science fiction"}
-# ])
+Genre.create!([
+  {name: "thriller"},
+  {name: "romcom"},
+  {name: "action"},
+  {name: "science fiction"}
+])
 
 MovieGenre.create!([
   {movie_id: 1},
